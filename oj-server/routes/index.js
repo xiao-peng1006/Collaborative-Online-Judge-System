7818845c4a02b8166b1cc93110c9d18c64f5e41c
Xiao Peng <cvcv101px@gmail.com>
2018-11-11 11:42:56 -0800

    Minor Change

--- oj-server/routes/index.js
+++ oj-server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-  res.sendFile('../public/index.html', {root: path.dirname(__dirname)}); // Avoid going above the root in Heroku
+  res.sendFile('/app/public/index.html');
 });
 
 module.exports = router;
