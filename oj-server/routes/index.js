f7bc5017fdb2029f2be50f7fe0c58b165101c5cd
Xiao Peng <cvcv101px@gmail.com>
2018-11-11 11:12:17 -0800

    Minor Change

--- oj-server/routes/index.js
+++ oj-server/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
+  console.log('index.html', { root: path.join(__dirname, '../public/')});
   res.sendFile('index.html', { root: path.join(__dirname, '../public/')})
 });
 
