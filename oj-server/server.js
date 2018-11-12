dddf1b5a0297fed80e45ef4f828f41cc3809c514
Xiao Peng <cvcv101px@gmail.com>
2018-11-12 08:25:51 -0800

    Minor Change

--- oj-server/server.js
+++ oj-server/server.js
@@ -23,7 +23,7 @@ const indexRouter = require('./routes/index');
 // });
 
 app.use('/api/v1', restRouter);
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // app.listen(3000, () => {
 //   console.log('App is listening to port 3000!');
